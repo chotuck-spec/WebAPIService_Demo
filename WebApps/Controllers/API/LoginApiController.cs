fcb86aab5d1a018bb22f53626f462461e1fda369
lee.1379 <lee.1379@hotmail.com>
2025-11-19 16:22:39 +0800

    update

---
+++ WebApps/Controllers/API/LoginApiController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApps.Controllers.API
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginApiController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetLogin([FromQuery] string userId, [FromQuery] string password)
+        {
+            // Simple demo validation
+            if (userId == "admin" && password == "123")
+            {
+                return Ok(new { message = "Login successful" });
+            }
+
+            return Unauthorized(new { message = "Invalid credentials" });
+        }
+    }
+}
